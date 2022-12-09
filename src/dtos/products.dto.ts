11d4bff15a3dab748b53717406b3d8ac948a79db
Rowan Tan <rowantyj@gmail.com>
2022-12-09 22:44:35 +0800

    added Product DTO;

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,12 +1,18 @@
-import {
-  IsString,
-  IsUrl,
-  IsDecimal,
-  IsInt,
-  IsDateString,
-} from 'class-validator';
+import { IsString, IsUrl, IsDecimal, IsInt, IsUUID } from 'class-validator';
 
 export class CreateProductDto {
+  @IsUUID()
+  public uuid: string;
   @IsString()
   public title: string;
+  @IsString()
+  public description: string;
+  @IsUrl()
+  public picture: string;
+  @IsString()
+  public currency: string;
+  @IsDecimal()
+  public price: number;
+  @IsInt()
+  public quantity: number;
 }
