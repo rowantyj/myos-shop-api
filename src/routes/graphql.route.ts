73268c3d6e422e166e68800bee514c9e914e22d7
Rowan Tan <rowantyj@gmail.com>
2022-12-10 15:00:17 +0800

    added graphql, apollo-server;

---
+++ src/routes/graphql.route.ts
@@ -0,0 +1,68 @@
+import { Router } from 'express';
+import { Routes } from '@interfaces/routes.interface';
+import { ApolloServer } from 'apollo-server';
+import { PrismaClient } from '@prisma/client';
+import ProductService from '@/services/products.service';
+
+class GraphqlRoute implements Routes {
+  public path = '/gql';
+  public router = Router();
+  public product = new ProductService();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private async initializeRoutes() {
+    const typeDefs = `
+    type Query {
+      info: String!
+      products: [Product!]!
+      findProduct(keyword: String!): [Product!]!
+    }
+
+    type Product {
+      id: ID!
+      uuid: String!
+      title: String!
+      description: String!
+      currency: String!
+      price: String!
+      quantity: Int!
+      picture: String!
+    }
+    `;
+
+    const resolvers = {
+      Query: {
+        info: () => `This is the API of a Hackernews Clone`,
+        products: async () => await this.product.findAllProduct(),
+        findProduct: async (parent, args, contextValue, info) =>
+          await this.product.findProduct(args.keyword),
+      },
+      Product: {
+        id: parent => parent.id,
+        uuid: parent => parent.uuid,
+        description: parent => parent.description,
+        title: parent => parent.title,
+        currency: parent => parent.currency,
+        price: parent => parent.price,
+        picture: parent => parent.picture,
+      },
+    };
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: {
+        PrismaClient,
+      },
+    });
+
+    server
+      .listen()
+      .then(({ url }) => console.log(`Server is running on ${url}`));
+  }
+}
+
+export default GraphqlRoute;
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import ProductsRoute from '@routes/products.route';
+import GraphqlRoute from '@routes/graphql.route';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
@@ -11,6 +12,7 @@ const app = new App([
   new IndexRoute(),
   new UsersRoute(),
   new ProductsRoute(),
+  new GraphqlRoute(),
   new AuthRoute(),
 ]);
 
